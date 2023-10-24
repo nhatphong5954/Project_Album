16021370f3b76841361afb888ed7c08c37359732
nhatphong5954 <pukinanasi27@gmail.com>
2023-10-24 10:13:04 +0700

    Main Activity build

--- app/src/main/java/com/example/project_album/MainActivity.java
+++ app/src/main/java/com/example/project_album/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     GridView gridview;
     TextView txtSoloMsg;
     ImageView imgSoloPhoto;
